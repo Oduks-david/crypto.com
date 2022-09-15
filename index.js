35abff32aeeee13b976ff5ee3dbf7eadc15aae00
Oduks-david <103524307+Oduks-david@users.noreply.github.com>
2022-09-15 16:24:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById('nav').style.transform = "translateX(0)"; 
+}
+
+function cancelNav() {
+    document.getElementById('nav').style.transform = "translateX(-100%)";
+}
+
---
+++ scroll.js
@@ -0,0 +1,132 @@
+ScrollReveal({
+    reset: true,
+    distance: '50px',
+    duration: '3000',
+    delay: 1000
+
+});
+
+ScrollReveal().reveal('.headline', 
+    { delay: 500, 
+      origin: 'bottom', 
+      interval: 200
+    });
+
+ScrollReveal().reveal('.free-button a i', 
+    { delay: 500, 
+      origin: 'left', 
+      interval: 200
+    });
+
+
+ScrollReveal().reveal('.free-button', 
+    { delay: 500, 
+      origin: 'top', 
+      interval: 200
+    });
+
+
+ScrollReveal().reveal('.save', 
+    { delay: 500, 
+      easing: 'ease-in', 
+      interval: 200
+    });
+
+
+ScrollReveal().reveal('.hero-image img', 
+    { delay: 500, 
+      origin: 'bottom', 
+      interval: 200
+    });
+
+
+ScrollReveal().reveal('.digital > div', 
+    { delay: 500, 
+      origin: 'bottom', 
+      interval: 200
+    });
+
+
+ScrollReveal().reveal('.chappo-headline', 
+    { delay: 500, 
+      origin: 'bottom', 
+      interval: 200, 
+       opacity: 0.2
+    });
+
+
+ScrollReveal().reveal('.chappo-image img',
+    { delay: 500, 
+      origin: 'bottom', 
+      interval: 200
+    });
+
+
+ScrollReveal().reveal('.earning h1', 
+    { delay: 1000, 
+      origin: 'bottom', 
+      interval: 600, 
+      opacity: 0.2
+    });
+
+
+ScrollReveal().reveal('.earning-text small',
+    { delay: 500, 
+      origin: 'bottom', 
+      interval: 200,  
+      opacity: 0.2
+    });
+
+
+ScrollReveal().reveal('.calculate-container', 
+    { delay: 500, 
+      origin: 'bottom', 
+      interval: 200, 
+      opacity: 0.2
+    });
+
+
+ScrollReveal().reveal('.trade-container h1', 
+   { delay: 500,
+     origin: 'bottom',
+     interval: 200,
+     opacity: 0.2
+    });
+
+
+ScrollReveal().reveal('.smart h1', 
+   { delay: 500,
+     interval: 200,  
+     opacity: 0.2
+    });
+
+
+ScrollReveal().reveal('.invest-image img',
+    { delay: 500,
+      origin: 'bottom', 
+      interval: 200, 
+      opacity: 0.2
+    });
+
+
+ScrollReveal().reveal('.smart p', 
+    { delay: 500, 
+      origin: 'bottom',
+      interval: 200,
+      opacity: 0.2
+    });
+
+
+ScrollReveal().reveal('.invest-button', 
+   { delay: 500, 
+     origin: 'left',
+     interval: 200,  
+     opacity: 0
+   });
+
+ScrollReveal().reveal('.seventh-section', 
+   { delay: 500, 
+     origin: 'bottom', 
+     interval: 200,  
+     opacity: 0.2
+   });
